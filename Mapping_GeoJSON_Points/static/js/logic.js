24339365261b9f2b51dff581dd3db7a6d7d1a9f9
Mark Esposito <markanthonyesposito@hotmail.com>
2021-06-01 10:55:20 -0500

    13.5.3

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("workingg");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([37.5, -122.5], 10);
+let map = L.map('mapid').setView([30, 30], 2);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -11,36 +11,24 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     accessToken: API_KEY
 });
 
-// Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "state":"California",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/marke0816/Mapping_Earthquakes/main/majorAirports.json";
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+  console.log(data);
+  L.geoJson(data, {
+    onEachFeature: function (feature, layer) {
+      layer.bindPopup("<h2>Airport code: " + feature.properties.faa + "</h2>" + "<h3>Airport name: " + feature.properties.name + "</h3>");
+    }
+  }).addTo(map);
+});
+// Creating a GeoJSON layer with the retrieved data.
+// L.geoJSON(airportData, {
+//   onEachFeature: function (feature, layer) {
+//     layer.bindPopup("<h1>" + feature.properties.name + "</h1>");
+//   }
+// }).addTo(map);
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
-
-// Grabbing our GeoJSON data.
-L.geoJson(sanFranAirport, {
-    // We turn each feature into a marker on the map.
-    onEachFeature: function(feature, layer) {
-      console.log(layer);
-      layer.bindPopup("<h2>Airport Code: " + feature.properties.faa + "</h2>" + "<p><b>Airport Name: " + feature.properties.name + "</b></p>");
-    }
-
-  }).addTo(map);
