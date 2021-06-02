fab7b22e36cdc12d4847dbe89c150d4f5b5b62b6
Mark Esposito <markanthonyesposito@hotmail.com>
2021-06-02 11:20:03 -0500

    13.6.1

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -23,9 +23,9 @@ let baseMaps = {
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [43.7, -79.3],
-  zoom: 11,
-  layers: [satelliteStreets]
+  center: [39.5, -98.5],
+  zoom: 3,
+  layers: [streets]
 });
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -41,14 +41,8 @@ let myStyle = {
     weight: 1
 }
 
-// Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
-    console.log(data);
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data, {
-    style: myStyle,
-    onEachFeature: function (feature, layer) {
-      layer.bindPopup("<h2>" + feature.properties.AREA_NAME + "</h2>");
-    }
-  }).addTo(map);
+  L.geoJson(data).addTo(map);
 });
